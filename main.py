7420b22e283d31c7e1b85798026a52907bdcc5cd
Vesselin Tonchev <veselin.tonchev@delasport.com>
2023-03-01 16:47:24 +0200

    iterate over categories error when open next_[age: 'ascii' codec can't encode characters in position 60-69: ordinal not in range(128)

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import requests
 from urllib.request import urlopen
  
 # VARIABLES 
-url = '' # 'https://bgfirms.info/'
-reqs = requests.get(url)
+base_url = 'https://bgfirms.info/%D0%BA%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D1%8F/'
+reqs = requests.get(base_url)
 soup = BeautifulSoup(reqs.text, 'html.parser')
 count = 0
 category = categories.list_all_categories
@@ -14,16 +14,16 @@ category = categories.list_all_categories
 urls = []
 
 for link in category:
-    url = category[link]
+    url = f'{base_url}{category[count]}'
+    count += 1
 
+    for link in soup.find_all('a'):
+        page = link.get('href')
+        # print("https://bgfirms.info", end='')
+        
+        next_page = urlopen(url)
+        html_bytes = next_page.read()
+        html = html_bytes.decode("utf-8")
 
-# for link in soup.find_all('a'):
-#     page = link.get('href')
-#     print("https://bgfirms.info", end='')
-    
-#     next_page = urlopen(url)
-#     html_bytes = next_page.read()
-#     html = html_bytes.decode("utf-8")
-
-#     print(html)
-    
+        print(html)
+        
