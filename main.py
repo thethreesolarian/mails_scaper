35374a468c947d4a38577d8aef73074c23da6cf3
thethreesolarian <90781762+thethreesolarian@users.noreply.github.com>
2023-03-12 12:12:52 +0100

    Working on excluding the main links

--- main.py
+++ main.py
@@ -2,28 +2,32 @@ from bs4 import BeautifulSoup
 import categories
 import requests
 from urllib.request import urlopen
- 
-# VARIABLES 
-base_url = 'https://bgfirms.info/%D0%BA%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D1%8F/'
+import urllib.parse
+
+# VARIABLES
+base_url = 'https://bgfirms.info/категория/'
 reqs = requests.get(base_url)
 soup = BeautifulSoup(reqs.text, 'html.parser')
 count = 0
 category = categories.list_all_categories
 
-# LOOP OVER ALL URLS 
+# LOOP OVER ALL URLS
 urls = []
 
 for link in category:
     url = f'{base_url}{category[count]}'
     count += 1
 
-    for link in soup.find_all('a'):
-        page = link.get('href')
-        # print("https://bgfirms.info", end='')
+    for inner_link in soup.find_all('a'):
+        page = inner_link.get('href')
+        if  page != '#' or  page != '/за-нас':
         
-        next_page = urlopen(url)
-        html_bytes = next_page.read()
-        html = html_bytes.decode("utf-8")
+            
+            # print("https://bgfirms.info", end='')
 
-        print(html)
-        
+            # url_encoded = urllib.parse.quote(url, safe='')
+            # next_page = urlopen(url_encoded)
+            # html_bytes = next_page.read()
+            # html = html_bytes.decode("utf-8", errors="ignore")
+
+            print(page)
